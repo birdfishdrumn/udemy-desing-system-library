6f3627a1d686ca2e8e300d357587e5c8ec56c690
michihiro <pomeofishbird0303@gmail.com>
2022-04-09 08:09:30 +0900

    デザインシステムの基盤を構築

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Button } from "@ds.e/react";
+
+import "@ds.e/scss/lib/Button.css";
+
+ReactDOM.render(<Button label='Test Buttton' />, document.querySelector("#root"));
